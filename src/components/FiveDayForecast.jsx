5676f55e6cd070db585a2a024a25701cbb12e88e
Soumya Mishra <soumyawowsoumya123@gmail.com>
2026-07-18 16:15:00 +0530

    icons added and layout ready of "FDF"

--- src/components/FiveDayForecast.jsx
+++ src/components/FiveDayForecast.jsx
@@ -1,8 +1,59 @@
 import { useState } from "react";
 import { WiRaindrop } from "react-icons/wi";
+import {
+  WiDaySunny,
+  WiNightClear,
+  WiDayCloudy,
+  WiNightAltCloudy,
+  WiCloud,
+  WiCloudy,
+  WiRain,
+  WiShowers,
+  WiThunderstorm,
+  WiSnow,
+  WiFog,
+  WiHumidity,
+  WiStrongWind,
+  WiBarometer,
+  WiSunrise,
+  WiSunset,
+  WiDayRain,
+  WiNightAltRain,
+  WiCelsius,
+} from "react-icons/wi";
 
 
 function FiveDayForecast({datafive}){
+
+    
+    const weatherIcons = {
+        "01d": WiDaySunny,
+        "01n": WiNightClear,
+
+        "02d": WiDayCloudy,
+        "02n": WiNightAltCloudy,
+
+        "03d": WiCloud,
+        "03n": WiCloud,
+
+        "04d": WiCloudy,
+        "04n": WiCloudy,
+
+        "09d": WiShowers,
+        "09n": WiShowers,
+
+        "10d": WiDayRain,
+        "10n": WiNightAltRain,
+
+        "11d": WiThunderstorm,
+        "11n": WiThunderstorm,
+
+        "13d": WiSnow,
+        "13n": WiSnow,
+
+        "50d": WiFog,
+        "50n": WiFog,
+    };
 
     
     const groupedForecast = datafive.list.reduce((acc, obj)=>{
@@ -13,7 +64,7 @@ function FiveDayForecast({datafive}){
         }
 
         acc[date] = [...acc[date], obj] 
-
+        
         return acc
     }, {})
 
@@ -24,6 +75,12 @@ function FiveDayForecast({datafive}){
         
         return day
     })
+
+    const weatherIcon = (icon) =>{
+        const TempIcon = weatherIcons[icon]
+
+        return <TempIcon/>
+    }
     
     const dates = Object.keys(groupedForecast)
     const [selectedDate, setSelectedDate] = useState(dates[0])
@@ -42,12 +99,14 @@ function FiveDayForecast({datafive}){
 
 
     const postcards = groupedForecast[selectedDate].map((obj)=>(
-        <div className="h-35 w-35 border rounded-2xl flex flex-col justify-center items-center">
+        <div className="h-40 w-40 border rounded-2xl flex flex-col justify-center items-center">
             <div className="text-2xl">{converter(obj.dt_txt)}</div>
-
-            <div className="text-4xl">{obj.main.temp}</div>
-            <div className="flex">
-                <WiRaindrop /> {obj.pop} %
+            <div className="text-7xl -my-2">{weatherIcon(obj.weather[0].icon)}</div>
+            <div className="flex flex-col items-center">
+                <div className="text-4xl">{obj.main.temp}</div>
+                <div className="flex">
+                    <WiRaindrop className="text-2xl"/> {obj.pop} %
+                </div>
             </div>
         </div>
     ))
@@ -55,20 +114,20 @@ function FiveDayForecast({datafive}){
 
 
     return(
-        <div className="forecastConatiner bg-white p-4">
-            <div className="flex justify-center border rounded-t-2xl  items-start">
-                <div className="flex gap-2 h-12 mt-1">
-                    <button className="border border-b-0 w-20 rounded-t-2xl" onClick={()=> setSelectedDate(dates[0])}>Today</button>
-                    <button className="border border-b-0 w-20 rounded-t-2xl" onClick={()=> setSelectedDate(dates[1])}>Tommorow</button>
-                    <button className="border border-b-0 w-20 rounded-t-2xl" onClick={()=> setSelectedDate(dates[2])}>{days[2]}</button>
-                    <button className="border border-b-0 w-20 rounded-t-2xl" onClick={()=> setSelectedDate(dates[3])}>{days[3]}</button>
-                    <button className="border border-b-0 w-20 rounded-t-2xl" onClick={()=> setSelectedDate(dates[4])}>{days[4]}</button>
+        <div className="forecastConatiner flex justify-center bg-white p-4">
+            <div className=" w-fit">
+                <div className="flex justify-center border rounded-t-2xl  items-start">
+                    <div className="flex gap-2 h-15 mt-1">
+                        <button className="border border-b-0 w-25 rounded-t-2xl text-xl" onClick={()=> setSelectedDate(dates[0])}>Today</button>
+                        <button className="border border-b-0 w-25 rounded-t-2xl text-xl" onClick={()=> setSelectedDate(dates[1])}>Tommorow</button>
+                        <button className="border border-b-0 w-25 rounded-t-2xl text-xl" onClick={()=> setSelectedDate(dates[2])}>{days[2]}</button>
+                        <button className="border border-b-0 w-25 rounded-t-2xl text-xl" onClick={()=> setSelectedDate(dates[3])}>{days[3]}</button>
+                        <button className="border border-b-0 w-25 rounded-t-2xl text-xl" onClick={()=> setSelectedDate(dates[4])}>{days[4]}</button>
+                    </div>
                 </div>
-            </div>
-
-            <div className="flex min-h-40 border border-t-0 rounded-b-2xl items-center px-4 gap-2">
-                {postcards}
-
+                <div className="grid grid-cols-8 min-h-50 border border-t-0 rounded-b-2xl items-center px-6 gap-6">
+                    {postcards}
+                </div>
             </div>
         </div>
     )
