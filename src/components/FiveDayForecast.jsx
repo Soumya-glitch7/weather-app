5afe396595e0068e2b98222fc37f6c196bffba01
Soumya Mishra <soumyawowsoumya123@gmail.com>
2026-07-19 00:19:55 +0530

    Five Day Forecast designing done

--- src/components/FiveDayForecast.jsx
+++ src/components/FiveDayForecast.jsx
@@ -121,13 +121,13 @@ function FiveDayForecast({datafive}){
 
     const postcards = groupedForecast[selectedDate].map((obj)=>(
         <div className="w-46 snap-start p-3">
-            <div className="h-40 w-40 border rounded-2xl flex flex-col justify-center items-center shrink-0">
+            <div className="h-40 w-40 border-0 rounded-2xl flex flex-col justify-center items-center shrink-0 bg-[#617e5f] text-white">
                 <div className="text-2xl">{converter(obj.dt_txt)}</div>
                 <div className="text-7xl -my-2">{weatherIcon(obj.weather[0].icon)}</div>
                 <div className="flex flex-col items-center">
                     <div className="text-4xl">{obj.main.temp}</div>
-                    <div className="flex">
-                        <WiRaindrop className="text-2xl"/> {obj.pop} %
+                    <div className="flex items-center">
+                        <WiRaindrop className="text-2xl -mb-1"/> {obj.pop} %
                     </div>
                 </div>
             </div>
@@ -157,18 +157,18 @@ function FiveDayForecast({datafive}){
 
 
     return(
-        <div className="forecastConatiner flex justify-center bg-white p-4">
+        <div className="forecastConatiner flex  p-4">
             <div className=" w-fit relative">
-                <div className="flex justify-center border rounded-t-2xl  items-start">
-                    <div className="flex gap-2 h-10 mt-1">
-                        <button className="border border-b-0 w-25 rounded-t-2xl" onClick={()=> setSelectedDate(dates[0])}>Today</button>
-                        <button className="border border-b-0 w-25 rounded-t-2xl" onClick={()=> setSelectedDate(dates[1])}>Tommorow</button>
-                        <button className="border border-b-0 w-25 rounded-t-2xl" onClick={()=> setSelectedDate(dates[2])}>{days[2]}</button>
-                        <button className="border border-b-0 w-25 rounded-t-2xl" onClick={()=> setSelectedDate(dates[3])}>{days[3]}</button>
-                        <button className="border border-b-0 w-25 rounded-t-2xl" onClick={()=> setSelectedDate(dates[4])}>{days[4]}</button>
+                <div className="flex justify-center border-0 rounded-t-2xl bg-[#070a0e]  items-start">
+                    <div className="flex gap-2 h-10 mt-1 text-white">
+                        <button className={`border w-25 bg-[#2e3f45] hover:bg-[#2e3f45ad] ${ (selectedDate === dates[0]) ? ("bg-[#191f23] rounded-t-2xl border-b-0") : ("bg-[#2e3f45] rounded-2xl") }`} onClick={()=> setSelectedDate(dates[0])}>Today</button>
+                        <button className={`border w-25 bg-[#2e3f45] hover:bg-[#2e3f45ad] ${ (selectedDate === dates[1]) ? ("bg-[#191f23] rounded-t-2xl border-b-0") : ("bg-[#2e3f45] rounded-2xl") } `} onClick={()=> setSelectedDate(dates[1])}>Tommorow</button>
+                        <button className={`border w-25 bg-[#2e3f45] hover:bg-[#2e3f45ad] ${ (selectedDate === dates[2]) ? ("bg-[#191f23] rounded-t-2xl border-b-0") : ("bg-[#2e3f45] rounded-2xl") }`} onClick={()=> setSelectedDate(dates[2])}>{days[2]}</button>
+                        <button className={`border w-25 bg-[#2e3f45] hover:bg-[#2e3f45ad] ${ (selectedDate === dates[3]) ? ("bg-[#191f23] rounded-t-2xl border-b-0") : ("bg-[#2e3f45] rounded-2xl") }`} onClick={()=> setSelectedDate(dates[3])}>{days[3]}</button>
+                        <button className={`border w-25 bg-[#2e3f45] hover:bg-[#2e3f45ad] ${ (selectedDate === dates[4]) ? ("bg-[#191f23] rounded-t-2xl border-b-0") : ("bg-[#2e3f45] rounded-2xl") }`} onClick={()=> setSelectedDate(dates[4])}>{days[4]}</button>
                     </div>
                 </div>
-                <div className="flex w-230 min-h-50 border border-t-0 rounded-b-2xl items-center overflow-x-auto snap-x snap-mandatory" onScroll={()=>updateButtons()} ref={containerRef}>
+                <div className="flex w-230 min-h-50 border border-t-0 rounded-b-2xl items-center overflow-x-hidden snap-x snap-mandatory bg-[#191f23]" onScroll={()=>updateButtons()} ref={containerRef}>
                     {postcards}
                 </div>
                 <button className={`absolute right-0 bottom-0 rounded-br-2xl h-50 w-8 bg-black/20 flex justify-center items-center ${showRight} `} onClick={scrollRight}><IoIosArrowForward /></button>
